918c5b5cd7b960351d3ec5c9bda42d2fe28be2d2
adut24 <dutertre.alexandre@laposte.net>
2023-04-03 15:03:17 +0200

    Insert a node in a binary tree

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - insert a node inside a binary tree
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = NULL;
+
+	newNode = malloc(sizeof(binary_tree_t));
+	/* If the creation of the new node fail, return NULL */
+	if (!newNode)
+		return (NULL);
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	/* If the pointer to the parent node is NULL, return the new node */
+	if (!parent)
+		return (newNode);
+
+	newNode->parent = parent;
+	return (newNode);
+}
