c1717e1323f94569d2003820a29b174e6c5d8a77
adut24 <dutertre.alexandre@laposte.net>
2023-04-06 09:47:35 +0200

    Insert a node in a Max Heap

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -12,18 +12,21 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
-
+};
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+heap_t *swap_value(heap_t *parent, heap_t *child);
+int get_size(heap_t *tree);
+heap_t *get_parent(heap_t *tree, int size, int index);
 
 #endif /* _BINARY_TREES_H_ */
