ad255daedc8178c517274a6884827a7c1fa88cfd
adut24 <dutertre.alexandre@laposte.net>
2023-04-10 12:37:46 +0200

    Find min operation to get to n

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module checking how many operations are needed to get n operations
+"""
+
+
+def minOperations(n):
+    """Function checking if it's possible to get n operations"""
+    file = 'H'
+    count = 0
+    clipboard = ''
+    
+    # First copy all
+    clipboard, count = copy_all(file, count)
+    # First paste
+    file, count = paste(file, clipboard, count)
+    while True:
+        length_file = len(file)
+        if length_file == n:
+            return count
+        if length_file > n:
+            return 0
+        if n % length_file == 0:
+            clipboard, count = copy_all(file, count)
+            file, count = paste(file, clipboard, count)
+        else:
+            file, count = paste(file, clipboard, count)
+
+
+def copy_all(file, count):
+    """Function copying the file and increasing the count"""
+    return file, count + 1
+
+
+def paste(file, clipboard, count):
+    """
+    Function pasting the content of the clipboard to the file.
+    Increase the count too.
+    """
+    return file + clipboard, count + 1
