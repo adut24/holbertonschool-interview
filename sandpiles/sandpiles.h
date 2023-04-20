8035ad7b465e8b74e1e0ca8e6b5267e639fc15ad
adut24 <dutertre.alexandre@laposte.net>
2023-04-20 11:40:40 +0200

    Put step printing again

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -2,6 +2,9 @@
 #define SANDPILES_H
 #include <stdio.h>
 
+/* Given functions */
+static void print_grid(int grid[3][3]);
+
 /* Function to code */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
