51583d1e1a255427e121929fc391016d81965fe3
adut24 <dutertre.alexandre@laposte.net>
2023-05-26 11:45:19 +0200

    Fix in case of multiple bytes for one number

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -6,16 +6,18 @@ def validUTF8(data):
     """Checks if the data set given is a valid UTF-8 encoding"""
     n_bytes = 0
     for num in data:
-        # If the number is less than 128, it's a single byte ASCII character
-        if n_bytes == 0 and num < 128:
-            continue
-        binary = format(num, '08b')
         if n_bytes == 0:
-            # Gets the number of bytes used for the current character
-            n_bytes = binary.find('0')
+            mask = 128
+            # Counting the ones at the start to know the number of bytes
+            while mask & num:
+                n_bytes += 1
+                mask = mask * 2
+            if n_bytes == 0:
+                continue
             if n_bytes == 1 or n_bytes > 4:
                 return False
-        elif binary[:2] != '10':
+        # If the first bit is not 1 and the second not 0
+        elif not (num & 128 and not (num & 64)):
             return False
         n_bytes -= 1
     return n_bytes == 0
