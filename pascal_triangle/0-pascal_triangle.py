ef354ffa66cbae357bf91907e81ed0684714589c
adut24 <dutertre.alexandre@laposte.net>
2023-11-06 13:43:27 +0100

    Add Pascal's Triangle project

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+
+    Args:
+    - n: size of the triangle (n x n)
+
+    Returns:
+    - list: list of lists of integers representing the Pascal’s triangle of n
+    """
+    triangle = []
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(row)
+    return triangle
